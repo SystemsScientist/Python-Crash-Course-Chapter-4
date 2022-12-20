f086a77fc7a5a789aa1fcb86ea276ec1c4d06d78
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-12-19 19:37:13 -0600

    Adding magicians.py file

---
+++ Examples/magicians.py
@@ -0,0 +1,10 @@
+# magicians.py, Chapter 04, Python Crash Course
+# program uses a for loop and prints the names
+# of magicians
+
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(magician)
+
+
+
