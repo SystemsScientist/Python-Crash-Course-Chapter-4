1b04ff533faf0cae811d6f5d6a71f46f048ab8e9
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-12-21 16:41:10 -0600

    Adding basic_stats.py file

---
+++ Examples/basic_stats.py
@@ -0,0 +1,13 @@
+# basic_stats.py, Chapter 4, Python Crash Course
+# program finds and prints the min, max, and sum
+# of a list of numbers
+#
+
+digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+
+print(min(digits))
+print(max(digits))
+print(sum(digits))
+
+
+
