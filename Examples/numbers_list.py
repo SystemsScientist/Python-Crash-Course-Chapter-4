7ad3529c41545a80ff92d45e82b6866191f7bba6
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-12-21 16:24:23 -0600

    Adding numbers_list.py file

---
+++ Examples/numbers_list.py
@@ -0,0 +1,11 @@
+# numbers_list.py, Chapter 4, Python Crash Course
+# program uses the range() function inside of the
+# list() function and prints a list of numbers 
+# using the print() function
+#
+
+numbers = list(range(1,6))
+print(numbers)
+
+
+
