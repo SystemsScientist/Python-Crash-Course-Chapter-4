f760a0eac555387a2d0f772566cd2cfbcd5e6c19
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-12-21 05:57:24 -0600

    Adding magicians7.py file

---
+++ Examples/magicians7.py
@@ -0,0 +1,10 @@
+# magicians7.py, Chapter 4, Python Crash Course
+# program produces a syntax error
+#
+
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians # does not include a colon
+    print(magician)
+
+
+
