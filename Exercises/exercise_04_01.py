b76c16e469583cf325d3d7e5cd8ab45c0003e4ff
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-12-21 06:17:37 -0600

    Adding exercise_04_01.py file

---
+++ Exercises/exercise_04_01.py
@@ -0,0 +1,36 @@
+# exercise_04_01.py, Chapter 04, Python Crash Course
+#
+# Pizzas: Think of at least three kinds of your favorite
+# pizza. Store these pizza names in a list, and then use
+# a for loop to print the name of each pizza.
+
+favorite_pizzas = ['pepperoni', 'cheese', 'vegetarian']
+for pizza in favorite_pizzas:
+    print(pizza.title())
+
+# - Modify your for loop to print a sentence using the name
+#   of the pizza instead of printing just the name of the 
+#   pizza. For each pizza you should have one line of output
+#   containing a simple statement like "I like pepperoni
+#   pizza"
+
+favorite_pizzas = ['pepperoni', 'cheese', 'vegetarian']
+for pizza in favorite_pizzas:
+    print("I love " + pizza.title() + " pizza!")
+    print(pizza.title() + " pizza is very tasty!")
+
+# - Add a line at the end of your program, outside the for
+#   loop, that states how much you like pizza. The output
+#   should consist of three or more lines about the kinds
+#   of pizza you like and then an additional sentence, such
+#   as "I really love pizza!"
+
+favorite_pizzas = ['pepperoni', 'cheese', 'vegetarian']
+for pizza in favorite_pizzas:
+    print("I love " + pizza.title() + " pizza!")
+    print(pizza.title() + " pizza is very tasty!")
+
+print("I really love tasty, pizza!")
+
+
+
