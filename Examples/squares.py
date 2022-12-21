daa958198b8608fe3b53a1655efda529d7007501
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-12-21 16:31:45 -0600

    Adding squares.py file

---
+++ Examples/squares.py
@@ -0,0 +1,13 @@
+# squares.py, Chapter 4, Python Crash Course
+# program prints the first 10 squares
+#
+
+squares = []
+for value in range(1,11):
+    square = value**2
+    squares.append(square)
+
+print(squares)
+
+
+
