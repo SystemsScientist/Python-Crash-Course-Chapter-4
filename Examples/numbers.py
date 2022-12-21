d6e98b25b5742c921b8eb94b797c9039cb24bc1c
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-12-21 07:03:00 -0600

    Adding numbers.py file

---
+++ Examples/numbers.py
@@ -0,0 +1,11 @@
+# numbers.py, Chapter 4, Python Crash Course
+# program uses the range() function and prints
+# a series of numbers using the for loop and 
+# print() function
+#
+
+for value in range(1,5):
+    print(value)
+
+
+
