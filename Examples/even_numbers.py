e4fbf86611e0f6f5110302e16d119f12cdd7daac
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-12-21 16:28:25 -0600

    Adding even_numbers.py file

---
+++ Examples/even_numbers.py
@@ -0,0 +1,11 @@
+# even_numbers.py, Chapter 4, Python Crash Course
+# program uses the range() function inside of the
+# list() function and prints a list of even numbers
+# using the print() function
+#
+
+even_numbers = list(range(2,11,2))
+print(even_numbers)
+
+
+
