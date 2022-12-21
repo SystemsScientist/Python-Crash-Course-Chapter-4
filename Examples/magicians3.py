bca6203a9dd1bdfff77ac4d81fffbc0e0d89d8e4
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-12-20 18:35:53 -0600

    Adding magicians3.py file

---
+++ Examples/magicians3.py
@@ -0,0 +1,11 @@
+# magicians3.py, Chapter 4, Python Crash Course
+# program uses a for loop to print a list of names
+# of magicians
+
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(magician.title() + ", that was a great trick!")
+    print("I can't wait to see your next trick, " + magician.title() + ".\n")
+
+
+
