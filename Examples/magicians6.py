33539fd30504d255a405ebcaf1e90b9873e8e45b
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-12-21 05:49:40 -0600

    Adding magicians6.py file

---
+++ Examples/magicians6.py
@@ -0,0 +1,16 @@
+# magicians6.py, Chapter 4, Python Crash Course
+# program uses a for loop to print a list of names
+# of magicians
+#
+# program also includes an unnecessary indentation
+#
+
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(magician.title() + ", that was a great trick!")
+    print("I can't wait to see your next trick, " + magician.title() + ".\n")
+
+    print("Thank you, everyone. That was a great magic show!")
+
+
+
