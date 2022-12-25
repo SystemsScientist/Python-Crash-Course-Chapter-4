3acfc2250301b1f30506be15ef094518ffd23601
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-12-25 07:39:59 -0600

    Adding exercise_04_03.py file

---
+++ Exercises/exercise_04_03.py
@@ -0,0 +1,10 @@
+# exercise_04_03.py, Chapter 4, Python Crash Course
+#
+# Counting to Twenty: Use a for loop to print the numbers
+# from 1 to 20, inclusive
+
+for integer in range(1,21):
+    print(integer)
+
+
+
