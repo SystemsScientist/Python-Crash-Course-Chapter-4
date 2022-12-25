047e800e7d2db37f1247cf6ab9e8f70b689fbd53
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-12-25 07:33:01 -0600

    Adding list_comprehension.py file

---
+++ Examples/list_comprehension.py
@@ -0,0 +1,9 @@
+# list_comprehension.py, Chapter 4, Python Crash Course
+# program uses list comprehension to print the first 10
+# square numbers
+
+squares = [value**2 for value in range(1,11)]
+print(squares)
+
+
+
