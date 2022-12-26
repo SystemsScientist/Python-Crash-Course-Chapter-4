006f07418b1ae44b7956ecb18eaf1f22fe06affb
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-12-26 17:50:09 -0600

    Adding players.py file

---
+++ Examples/players.py
@@ -0,0 +1,8 @@
+# players.py, Chapter 4, Python Crash Course
+# program prints a slice of a list of players
+
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+print(players[0:3])
+
+
+
