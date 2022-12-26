35aed28ba7732d8e3cd5df648c0285bbfc45f980
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-12-26 17:45:27 -0600

    Adding exercise_04_09.py file

---
+++ Exercises/exercise_04_09.py
@@ -0,0 +1,10 @@
+# exercise_04_09.py, Chapter 4, Python Crash Course
+#
+# Cube Comprehension: Use a list comprehension to
+# generate a list of the first 10 cubes.
+
+cubes = [value ** 3 for value in range(1,11)]
+print(cubes)
+
+
+
