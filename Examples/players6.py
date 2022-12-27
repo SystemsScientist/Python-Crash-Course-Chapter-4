1ce6bb3298d2f14dcbceb153a5afc7bc9e212b77
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-12-26 18:17:30 -0600

    Adding players6.py file

---
+++ Examples/players6.py
@@ -0,0 +1,12 @@
+# players6.py, Chapter 4, Python Crash Course
+# program uses a for loop to print a subset of players
+# from the list of players
+
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+
+print("Here are the first three players on my team: ")
+for player in players[:3]:
+    print(player.title())
+
+
+
