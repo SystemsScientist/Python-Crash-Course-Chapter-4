f30468213bb831340ea4bbda432ee427fefec2d3
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-12-26 18:25:19 -0600

    Adding foods.py file

---
+++ Examples/foods.py
@@ -0,0 +1,14 @@
+# foods.py, Chapter 4, Python Crash Course
+# program prints a list of foods
+
+my_foods = ['pizza', 'falafel', 'carrot cake']
+friend_foods = my_foods[:]
+
+print("My favorite foods are: ")
+print(my_foods)
+
+print("\nMy friend's favorite foods are: ")
+print(friend_foods)
+
+
+
