a5639e37e3a84bb50cf3cd289aced7d73b22929b
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-12-26 18:13:04 -0600

    Adding players5.py file

---
+++ Examples/players5.py
@@ -0,0 +1,8 @@
+# players5.py, Chapter 4, Python Crash Course
+# program prints the last three players from the list
+
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+print(players[-3:])
+
+
+
