4a302f77f4337ace03f3e3fa7a37f4bc0ca9ff20
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-12-26 18:39:19 -0600

    Adding foods2.py file

---
+++ Examples/foods2.py
@@ -0,0 +1,18 @@
+# foods2.py, Chapter 4, Python Crash Course
+# program adds foods to two different lists
+# and then prints the lists
+
+my_foods = ['pizza', 'falafel', 'carrot cake']
+friend_foods = my_foods[:]
+
+my_foods.append('cannoli')
+friend_foods.append('ice cream')
+
+print("My favorite foods are: ")
+print(my_foods)
+
+print("\nMy friend's favorite foods are: ")
+print(friend_foods)
+
+
+
