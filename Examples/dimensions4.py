591dc104f1a53da8520e1fc2116dd48267f230dd
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-01-02 18:04:19 -0600

    Adding dimensions4.py file

---
+++ Examples/dimensions4.py
@@ -0,0 +1,16 @@
+# dimensions4.py, Chapter 4, Python Crash Course
+# program demonstrates how to redefine a tuple 
+# using a for loop
+
+dimensions = (200, 50)
+print("Original dimensions: ")
+for dimension in dimensions:
+    print(dimension)
+
+dimensions = (400, 50)
+print("\nModified dimensions: ")
+for dimension in dimensions:
+    print(dimension)
+
+
+
