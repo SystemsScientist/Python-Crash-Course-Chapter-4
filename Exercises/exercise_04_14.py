94d6a870970a0142cb77e643707a93beb5893fbd
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-01-02 18:32:20 -0600

    Adding exercise_04_14.py file

---
+++ Exercises/exercise_04_14.py
@@ -0,0 +1,13 @@
+# exercise_04_14.py, Chapter 4, Python Crash Course
+# 
+# PEP 8: Look through the original PEP 8 style guide
+# at https://python.org/dev/peps/pep-0008/. You won't
+# use much of it now, but it might be interesting to
+# skim through it.
+
+pep_8 = "https://python.org/dev/peps/pep-0008/"
+
+print("\nPEP 8: " + pep_8 + "\n")
+
+
+
